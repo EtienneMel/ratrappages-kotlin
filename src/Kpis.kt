b6a389149429eef41c647c40d39d045eb61b1ebc
EtienneMel <etienne.mela@devinci.fr>
2019-02-10 13:54:48 +1100

    Lecture correcte, data types OK

--- src/Kpis.kt
+++ src/Kpis.kt
@@ -1,25 +1,13 @@
 package com.javasampleapproach.kotlin.csv
 
-class Kpis {
-    var annee: String
-    var mois: String
-    var appareil: String
-    var commandes: String
-    var impressions: String
-    var clics: String
-    var couts: String
-    var chiffreAffaire: String
-
-    constructor(annee: String, mois: String, appareil: String, commandes: String, impressions: String, clics: String, couts: String, chiffreAffaire: String) {
-        this.annee = annee
-        this.mois = mois
-        this.appareil = appareil
-        this.commandes = commandes
-        this.impressions = impressions
-        this.clics = clics
-        this.couts = couts
-        this.chiffreAffaire = chiffreAffaire
-    }
+data class Kpis(var annee: Int,
+           var mois: String,
+           var appareil: String,
+           var commandes: Number,
+           var impressions: Int,
+           var clics: Int,
+           var couts: Number,
+           var chiffreAffaire: Number) {
 
     override fun toString(): String {
         return "[annee=" + annee + ", mois=" + mois + ", appareil=" + appareil + ", commandes=" + commandes + ", impressions=" + impressions + ", clics=" + clics + ", couts=" + couts + ", CA=" + chiffreAffaire + "]"
