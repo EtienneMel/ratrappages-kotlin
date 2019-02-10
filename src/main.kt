9723aa4ddfb12dc39c00a2b8e4ec4121e38b3b1d
Richardkev <richardkevin88@gmail.com>
2019-02-11 08:17:56 +1100

    Impressions des résultats OK

--- src/main.kt
+++ src/main.kt
@@ -53,14 +53,53 @@ fun main(args: Array<String>?) {
 
             line = fileReader.readLine()
         }
-
-
         // Add it to the maps
         for (ligne in kpis1) {
             initialiserMapAnnees(mapAnnees, ligne.annee, ligne.mois, ligne.chiffreAffaire)
             initialiserMapAppareils(mapAppareils, ligne.appareil, ligne.chiffreAffaire)
             initialiserMapAppareilsParMois(mapAppareilsParMois, ligne.appareil, ligne.mois, ligne.chiffreAffaire)
         }
+
+        // Lecture dans la console
+
+            //Panier moyen
+        println("------------ Panier moyen ------------")
+        val chiffreAffaireTotal = kpis1.map { it.commandes.toFloat() }.sum()
+        println("Panier moyen : " + formatFloat("%.2f", chiffreAffaireTotal / kpis1.size) + "€")
+
+            // Cout par clic
+        println("------------ Cout par clic ------------")
+        val clicsAuTotal = kpis1.map { it.clics.toFloat() }.sum()
+        val coutsAuTotal = kpis1.map { it.couts.toFloat() }.sum()
+        println("Cout par clics : " + formatFloat("%.2f", coutsAuTotal / clicsAuTotal) + "€")
+
+            //Taux de clic
+        println("------------ Taux de clic ------------")
+        val impressionsTotales = kpis1.map { it.impressions }.sum()
+        val taux = formatFloat("%.2f", (clicsAuTotal.toFloat() / impressionsTotales.toFloat()) * 100)
+        println("Taux de clic : $taux%")
+
+            //ROI
+        println("------------ Retour sur investissement ------------")
+        val roi = formatFloat("%.2f", chiffreAffaireTotal / coutsAuTotal * 100)
+        println("ROI : $roi%")
+
+            // CA par mois par année
+        println("------------ CA par mois par année ------------")
+
+        for ((annees, moisMap) in mapAnnees) {
+            for ((mois, chiffreAffaire) in moisMap) {
+                println(" Le Chiffre d'affaire de $mois / $annees est de " + formatFloat("%.2f", chiffreAffaire) + "€")
+            }
+        }
+            // CA par appareil
+        println("------------ CA par appareil ------------")
+
+        for ((appareil, chiffreAffaire) in mapAppareils) {
+            println(" Le chiffre d'Affaire pour l'appareil $appareil est de : " + formatFloat("%.2f", chiffreAffaire) + "€")
+        }
+
+        //EXCEPTIONS
 
     } catch (e: Exception) {
         println("Reading CSV Error!")
@@ -75,6 +114,12 @@ fun main(args: Array<String>?) {
     }
 }
 
+//Transformation au format float
+fun formatFloat(format: String, value: Number): String {
+    return format.format(value)
+}
+
+//Ajout des MAPS
 fun initialiserMapAnnees(map: MutableMap<Int, MutableMap<String, Number>>, annee: Int, mois: String, valeur: Number)
 {
     var listeAnnees = mutableMapOf<String, Number>()
